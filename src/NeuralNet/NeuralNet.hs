860d2ac3a86d8faa6bed6b2daea0e692871d4d2b
Daniel Holmes <daniel@danielholmes.org>
2018-02-05 22:04:59 +1100

    W initialisation for layers

--- src/NeuralNet/NeuralNet.hs
+++ src/NeuralNet/NeuralNet.hs
@@ -8,6 +8,7 @@ module NeuralNet.NeuralNet (
   layerActivation
 ) where
 
+import System.Random
 import Data.Matrix
 import NeuralNet.Activation
 
@@ -23,17 +24,25 @@ data NeuralNet = NeuralNet [NeuronLayer]
   deriving (Show, Eq)
 
 type NumInputs = Int
--- W = layer x
-buildNet :: NumInputs -> [LayerDefinition] -> NeuralNet
-buildNet _ [] = error "No layer definitions provided"
-buildNet numInputs layerDefs
-  | numInputs > 0 = NeuralNet (buildNeuronLayers numInputs layerDefs)
+
+buildNet :: StdGen -> NumInputs -> [LayerDefinition] -> NeuralNet
+buildNet _ _ [] = error "No layer definitions provided"
+buildNet g numInputs layerDefs
+  | numInputs > 0 = NeuralNet (fst (buildNeuronLayers g numInputs layerDefs))
   | otherwise     = error "Need positive num inputs"
 
-buildNeuronLayers :: Int -> [LayerDefinition] -> [NeuronLayer]
-buildNeuronLayers _ [] = []
-buildNeuronLayers numInputs (LayerDefinition a numNeurons:ds) = (NeuronLayer a w 0) : buildNeuronLayers numNeurons ds
-  where w = zero numInputs numNeurons
+buildNeuronLayers :: StdGen -> Int -> [LayerDefinition] -> ([NeuronLayer], StdGen)
+buildNeuronLayers g _ [] = ([], g)
+buildNeuronLayers g numInputs (LayerDefinition a numNeurons:ds) = (NeuronLayer a w 0 : ls, newG2)
+  where
+    foldStep :: Int -> ([Double], StdGen) -> ([Double], StdGen)
+    foldStep _ (accu, foldG) = (d:accu, newFoldG)
+      where (d, newFoldG) = randomR (0.0, 1.0) foldG
+
+    seeds = [1..(numInputs * numNeurons)]
+    (nums, newG) = foldr foldStep ([], g) seeds
+    w = fromList numInputs numNeurons nums
+    (ls, newG2) = buildNeuronLayers newG numNeurons ds
 
 nnLayers :: NeuralNet -> [NeuronLayer]
 nnLayers (NeuralNet layers) = layers
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,44 +3,55 @@ import NeuralNet.NeuralNet
 import NeuralNet.Activation
 import Control.Exception (evaluate)
 import Data.Matrix
+import Data.List
+import System.Random
 
 matrixSize :: Matrix Double -> (Int, Int)
 matrixSize m = (nrows m, ncols m)
 
+allUnique :: (Eq a) => Matrix a -> Bool
+allUnique m = length (nub list) == length list
+  where list = toList m
+
 main :: IO ()
-main = hspec $ do
-    describe "buildNet" $ do
-        it "returns correctly for small definition" $
-            let
-              def = [LayerDefinition ReLU 5, LayerDefinition Tanh 1, LayerDefinition Sigmoid 2]
-              nn = buildNet 8 def
-              layers = nnLayers nn
-            in do
-              layerActivation (layers!!0) `shouldBe` ReLU
-              layerActivation (layers!!1) `shouldBe` Tanh
-              layerActivation (layers!!2) `shouldBe` Sigmoid
-              matrixSize (layerW (layers!!0)) `shouldBe` (8, 5)
-              matrixSize (layerW (layers!!1)) `shouldBe` (5, 1)
-              matrixSize (layerW (layers!!2)) `shouldBe` (1, 2)
+main = do
+  g <- getStdGen
+  hspec $ do
+      describe "buildNet" $ do
+          it "returns correctly for small definition" $
+              let
+                def = [LayerDefinition ReLU 5, LayerDefinition Tanh 1, LayerDefinition Sigmoid 2]
+                nn = buildNet g 8 def
+                layers = nnLayers nn
+              in do
+                layerActivation (layers!!0) `shouldBe` ReLU
+                layerActivation (layers!!1) `shouldBe` Tanh
+                layerActivation (layers!!2) `shouldBe` Sigmoid
+                matrixSize (layerW (layers!!0)) `shouldBe` (8, 5)
+                matrixSize (layerW (layers!!1)) `shouldBe` (5, 1)
+                matrixSize (layerW (layers!!2)) `shouldBe` (1, 2)
+                layerW (layers!!0) `shouldSatisfy` allUnique
+                layerW (layers!!1) `shouldSatisfy` allUnique
+                layerW (layers!!2) `shouldSatisfy` allUnique
 
-        it "returns correctly for single layer definition" $
-            let
-              def = [LayerDefinition ReLU 5]
-              nn = buildNet 6 def
-              layers = nnLayers nn
-            in do
-              layerActivation (layers!!0) `shouldBe` ReLU
-              matrixSize (layerW (layers!!0)) `shouldBe` (6, 5)
+          it "returns correctly for single layer definition" $
+              let
+                def = [LayerDefinition ReLU 5]
+                nn = buildNet g 6 def
+                layers = nnLayers nn
+              in do
+                layerActivation (layers!!0) `shouldBe` ReLU
+                matrixSize (layerW (layers!!0)) `shouldBe` (6, 5)
 
-        it "throws exception for no layers" $
-            evaluate (buildNet 6 []) `shouldThrow` anyException
+          it "throws exception for no layers" $
+              evaluate (buildNet g 6 []) `shouldThrow` anyException
 
-        it "throws exception for no inputs" $
-            evaluate (buildNet 0 [LayerDefinition ReLU 5]) `shouldThrow` anyException
+          it "throws exception for no inputs" $
+              evaluate (buildNet g 0 [LayerDefinition ReLU 5]) `shouldThrow` anyException
 
-    describe "parseMessageWithType" $ do
-        it "returns Unknown for random" $
-            True `shouldBe` True
+      describe "parseMessageWithType" $ do
+          it "returns Unknown for random" $
+              True `shouldBe` True
 
-        it "returns Unknown for no timestamp" $
-            False `shouldBe` False
+          it "returns Unknown for no timestamp" $
+              False `shouldBe` False
